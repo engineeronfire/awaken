63028208f463d1b2c8f6335068a50d14614034b5
engineeronfire <engineeronfire@gmail.com>
2016-07-03 11:48:16 +0000

    login done,go home to finish

--- app/views.py
+++ app/views.py
@@ -27,9 +27,24 @@ def index():
                             title=None,
                             user=user,
                             posts=posts)
-
+# we have imported our LoginForm class, instantiated an object from it,
+#and sent it down to the template. This is all that is required to get form fields rendered.
+#the methods argument in the route decorator.
+#This tells Flask that this view function accepts GET and POST requests.
+#Without this the view will only accept GET requests. We will want to receive the POST requests, 
+#these are the ones that will bring in the form data entered by the user.
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+# If you call it when the form is being presented to the user
+#(i.e. before the user got a chance to enter data on it) then it will return False
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s". remember_me=%s' %
+                (form.openid.data, str(form.remember_me.data)))
+# redirect. This function tells the client web browser to navigate to a different page instead of the one requested.
+        return redirect('/index')
     return render_template('login.html',
                             title='Sign In',
                             form=form)
+
+
