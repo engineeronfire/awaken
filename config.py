a7cab92e49b6a249cef5c0a41dae08d68846aedd
engineeronfire <engineeronfire@gmail.com>
2016-07-04 11:54:51 +0000

    part4

--- config.py
+++ config.py
@@ -6,4 +6,12 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY =  'you-will-never-guess'
 
+#defining the list of OpenID providers that we want to present.
+#use this array in login view function
+OPENID_PROVIDERS = [
+
+        {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+        {'name': 'AOL', 'url': 'https://openid.aol.com/<username>'},
+        {'name': 'Flickr', 'url': 'https://www.flickr.com/<username>'},
+        {'name': 'MyopenId', 'url': 'https://www.myopenid.com'}]
 
